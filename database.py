9b4f2494f947cff251d789a3fe79f3228b77c12b
pujareddy2 <middepuja1005@gmail.com>
2026-03-20 11:50:02 +0530

    deploy product hub

--- database.py
+++
@@ -1,9 +0,0 @@
-
-from sqlalchemy.orm  import sessionmaker
-from sqlalchemy import create_engine
-
-
-db_url = "postgresql://postgres:puja@localhost:5555/mydata"
-engine= create_engine(db_url)
-Session = sessionmaker(autocommit=False,autoflush=False,bind=engine)
-
